081072b7f44b4ce38886e6c0a16ebce7bb220931
Hanni@PaulLaptop <hannipaulhenning@gmail.com>
2024-10-05 23:31:21 +0300

    week6

---
+++ RandomElement/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] snacks = { "sush", "pizza", "burger", "chicken wing", "Asian takeaway" };
+
+Random rnd = new Random();
+
+int randomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonigth we are going to eat {snacks[randomIndex]}.");
